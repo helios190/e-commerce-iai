2a7feb5e45a13942b701ace1b9f28f884602954e
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-06-04 04:26:14 +0700

    update edit product

--- server/API-barang/controllers/saveEditProduct.js
+++ server/API-barang/controllers/saveEditProduct.js
@@ -2,18 +2,22 @@ const Product = require("../models/barangDB.js");
 
 const saveEditProduct = async (req, res) => {
   const id = req.params.id || req.query.id;
-  const size = req.body.size;
+  const newSize = req.body.size;
 
   try {
-    const updatedProduct = await Product.findOneAndUpdate(
-      { _id: id },
-      { $set: { size } },
-      { new: true }
-    );
-
-    if (!updatedProduct) {
+    const product = await Product.findById(id);
+    if (!product) {
       return res.status(404).json({ error: "Product not found" });
     }
+
+    // Update the sizes without resetting unspecified sizes to 0
+    const updatedSize = { ...product.size, ...newSize };
+
+    const updatedProduct = await Product.findOneAndUpdate(
+      { _id: id },
+      { size: updatedSize },
+      { new: true }
+    );
 
     res.json(updatedProduct);
   } catch (error) {
